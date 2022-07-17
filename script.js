37ef68367d0c40ba58f8c340e776f203707ef116
S0fft <pelikh123.evg@gmail.com>
2022-07-17 12:57:12 +0200

    firstCommit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let num1 = 0;
+
+while (num1 < 10) {
+    console.log(num1);
+    num1++; 
+}
+
+// do {
+//     console.log(num1);
+//     num1++
+// } while (num1 < 10);
+
+// for (num1; num1 < 10; num1++) {
+//     console.log(num1);
+// }
